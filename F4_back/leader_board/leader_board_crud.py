385b69f37309f11015b8738a3d32284f3d67efae
ye0nda <yeonda@pusan.ac.kr>
2025-07-06 21:11:11 +0900

    7/6 리더보드4

--- F4_back/leader_board/leader_board_crud.py
+++ F4_back/leader_board/leader_board_crud.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from models import Game_session, User
 from database import get_db
 
@@ -7,13 +8,25 @@ def get_top_users_from_game_sessions(limit: int = 3):
     game_session_db = next(get_db("game_session"))
 
     try:
-        return (
-            game_session_db.query(User.user_name, Game_session.user_score)
-            .join(User, Game_session.user_id == User.user_id)
-            .order_by(Game_session.user_score.desc())
+        subquery = (
+            game_session_db.query(
+                Game_session.user_id,
+                func.max(Game_session.user_score).label("max_score")
+            )
+            .group_by(Game_session.user_id)
+            .subquery()
+        )
+
+        results = (
+            user_db.query(User.user_name, subquery.c.max_score)
+            .join(subquery, subquery.c.user_id == User.user_id)
+            .order_by(subquery.c.max_score.desc())
             .limit(limit)
             .all()
         )
+
+        return results
+
     finally:
         user_db.close()
         game_session_db.close()
