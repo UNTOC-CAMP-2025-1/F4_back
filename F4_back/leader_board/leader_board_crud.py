b57ebcf88be0f17e52a4adb6737eea8f22a68e01
ye0nda <yeonda@pusan.ac.kr>
2025-07-06 21:01:49 +0900

    7/6 리더보드

--- F4_back/leader_board/leader_board_crud.py
+++ F4_back/leader_board/leader_board_crud.py
@@ -1,11 +1,16 @@
 from sqlalchemy.orm import Session
 from models import Leader_board, User
+from database import get_db
 
 def get_top_users(db: Session, limit: int = 3):
-    return (
-        db.query(User.user_name, Leader_board.user_score)
-        .join(User, Leader_board.user_id == User.user_id)
-        .order_by(Leader_board.user_score.desc())
-        .limit(limit)
-        .all()
-    )
+    user_db = next(get_db("user"))
+    try:
+        return (
+            user_db.query(User.user_name, Leader_board.user_score)
+            .join(Leader_board, Leader_board.user_id == User.user_id)
+            .order_by(Leader_board.user_score.desc())
+            .limit(limit)
+            .all()
+        )
+    finally:
+        user_db.close()
