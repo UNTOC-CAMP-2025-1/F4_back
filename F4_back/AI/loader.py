e5ee1d3fd42a47f528b7027b28ceaf5eda633096
dajjangg <dahyung0216@naver.com>
2025-07-09 04:16:14 +0900

    7/9 대문자를 소문자로

--- F4_back/AI/loader.py
+++ F4_back/AI/loader.py
@@ -4,9 +4,9 @@ import numpy as np
 weights = np.load("~/untocF4/F4_back/F4_back/AI/dqn_weights.npz")
 
 # 각 레이어의 가중치와 편향
-W1, b1 = weights['W1'], weights['b1']
-W2, b2 = weights['W2'], weights['b2']
-W3, b3 = weights['W3'], weights['b3']
+w1, b1 = weights["w1"], weights["b1"]
+w2, b2 = weights["w2"], weights["b2"]
+w3, b3 = weights["w3"], weights["b3"]
 
 # ReLU 함수 정의
 def relu(x):
@@ -16,8 +16,8 @@ def relu(x):
 def predict_direction(state_x, state_y, player_x, player_y):
     x = np.array([[state_x, state_y, player_x, player_y]])  # shape: (1, 4)
 
-    x = relu(np.dot(x, W1) + b1)
-    x = relu(np.dot(x, W2) + b2)
-    x = np.dot(x, W3) + b3
+    x = relu(np.dot(x, w1) + b1)
+    x = relu(np.dot(x, w2) + b2)
+    x = np.dot(x, w3) + b3
 
     return int(np.argmax(x))  # 가장 높은 Q값의 인덱스를 행동으로 반환
