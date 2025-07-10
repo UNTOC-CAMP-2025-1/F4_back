c3b5c2c2a19e0487ef855a8a8c1640171c1f4461
dajjangg <dahyung0216@naver.com>
2025-07-11 05:48:52 +0900

    7/11 가중치 함수 토큰

--- F4_back/AI_bot/AI_bot_router.py
+++ F4_back/AI_bot/AI_bot_router.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, UploadFile, File
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 from AI_bot.AI_bot_schema import AIBotResponse, AIBotCreate
 from AI_bot.AI_bot_crud import create_ai_bot
 from AI_bot.util import get_db_by_domain
-from user.auth import get_current_user_id
+from user.auth import get_current_user_id, decode_access_token
 from database import get_db
 from functools import partial
 from AI_bot.AI_bot_crud import upload_weights
@@ -20,9 +21,18 @@ def create_ai_bot_endpoint(
     bot = create_ai_bot(db, bot_data, user_id=user_id)
     return bot
 
+security = HTTPBearer()
+
 @router.post("/upload_weights")
 def upload_ai_weights(
     weights: dict,
-    user_id: int = Depends(get_current_user_id)
+    authorization: HTTPAuthorizationCredentials = Depends(security)
 ):
+    token = authorization.credentials
+    payload = decode_access_token(token)
+
+    if not payload:
+        raise HTTPException(status_code=401, detail="토큰이 유효하지 않습니다.")
+
+    user_id = int(payload.get("sub"))
     return upload_weights(user_id, weights)
